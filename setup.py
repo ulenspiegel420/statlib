818ef4bb3cde0f7985e837ba23c9d551bd98d21a
Vlad Koval <itsvlad.koval@gmail.com>
2020-11-17 09:12:30 +0300

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='statlib',
-    version='0.1',
+    version='0.1.1',
     packages=['statlib'],
     url='',
     license='',
     author='Vlad Koval',
     author_email='itsvlad.koval@gmail.com',
-    description=''
+    description='Send stats to any sources'
 )
